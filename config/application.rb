cfc08d2f4648bdd38e4fc1b22a7fef3dc1d809c1
Andy Waite <github.aw@andywaite.com>
2014-04-12 18:15:07 +0200

    Avoid deprecation warning
    Avoids the warning:
    
    [deprecated] I18n.enforce_available_locales will default to true in the
    future. If you really want to skip validation of your locale you can set
    I18n.enforce_available_locales = false to avoid this message.
    
    See http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Techradar
       g.helper_specs false
     end
 
+    config.i18n.enforce_available_locales = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
