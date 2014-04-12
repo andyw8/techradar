d5dc2314c889b92f82c3ae7893aceb9763b51d55
Andy Waite <github.aw@andywaite.com>
2014-04-12 13:20:16 +0200

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,18 @@ Bundler.require(:default, Rails.env)
 
 module Techradar
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      
+      
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
