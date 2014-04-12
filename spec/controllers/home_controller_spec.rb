806d808a103d948823d5695252ce7ab7d7cdbc99
Andy Waite <github.aw@andywaite.com>
2014-04-12 13:20:49 +0200

    rails_apps_composer: rspec files

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe HomeController do
 
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
 end
