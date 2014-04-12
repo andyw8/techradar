02668a2d0f79a74da99480e52796514d0590ca34
Andy Waite <github.aw@andywaite.com>
2014-04-12 13:29:28 +0200

    Configure mailer for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Techradar::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
