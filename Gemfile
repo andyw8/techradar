3d8fb98a3f1ac2bbd94206c110f2174e27ec1bde
Andy Waite <github.aw@andywaite.com>
2014-04-15 21:13:03 +0200

    Add and configure letter_opener gem

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development do
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :mri_21, :rbx]
   gem 'foreman'
   gem 'html2haml'
+  gem 'letter_opener'
   gem 'quiet_assets'
   gem 'rails_layout'
   gem 'rubocop', require: false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Techradar::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -27,19 +24,9 @@ Techradar::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: ENV["DOMAIN_NAME"],
-    authentication: "plain",
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"]
-  }
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
-
 end
