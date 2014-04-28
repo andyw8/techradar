444233fa632baa2d4a3c4b4f4eb15bb2fb01ac61
Andy Waite <github.aw@andywaite.com>
2014-04-28 22:35:10 +0200

    Add admin field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -35,12 +35,12 @@ ActiveRecord::Schema.define(version: 20140428193611) do
   add_index "radars", ["owner_id"], name: "index_radars_on_owner_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
