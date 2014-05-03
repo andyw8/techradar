12295327e9cb699712c1ad53dc1ae7ae98cc701d
Andy Waite <github.aw@andywaite.com>
2014-05-03 14:02:40 +0200

    Fix DB seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file .env file.
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email name: ENV['ADMIN_NAME'].dup, email: ENV['ADMIN_EMAIL'].dup, password: ENV['ADMIN_PASSWORD'].dup, password_confirmation: ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.confirm!
+raise "users exist" if User.count > 0
+
+admin = User.create! name: ENV['ADMIN_NAME'].dup,
+  email: ENV['ADMIN_EMAIL'].dup,
+  password: ENV['ADMIN_PASSWORD'].dup,
+  password_confirmation: ENV['ADMIN_PASSWORD'].dup,
+  admin: true
+puts "Admin: #{admin.email}"
