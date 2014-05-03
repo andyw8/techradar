3b85c312f760133a22f76b3b154ebc06675bd86d
Andy Waite <github.aw@andywaite.com>
2014-05-03 14:18:25 +0200

    Whitespace

--- spec/models/blip_spec.rb
+++ spec/models/blip_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Blip do
   it { should belong_to(:radar) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:radar) }
   it { should validate_presence_of(:quadrant) }
 end
-
