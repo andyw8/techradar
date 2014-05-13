bc03f1c49458be4000d4500e62e16663b2e147d8
Andy Waite <github.aw@andywaite.com>
2014-05-13 21:13:20 +0200

    Re-order rings to match normal progression

--- app/models/blip.rb
+++ app/models/blip.rb
@@ -1,6 +1,6 @@
 class Blip < ActiveRecord::Base
   QUADRANTS = %w{techniques tools platforms languages_and_frameworks}
-  RINGS = %w{adopt trial assess hold}
+  RINGS = %w{assess trial adopt hold}
 
   belongs_to :radar
   belongs_to :topic
