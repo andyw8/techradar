b946281d504a4d5724786cbe9ddc9196ea7227d5
Andy Waite <github.aw@andywaite.com>
2014-05-18 13:40:08 +0200

    Formatting

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -16,8 +16,20 @@ FriendlyId.defaults do |config|
   # undesirable to allow as slugs. Edit this list as needed for your app.
   config.use :reserved
 
-  config.reserved_words = %w(new edit index session login logout users admin
-    stylesheets assets javascripts images)
+  config.reserved_words = %w(
+    admin
+    assets
+    edit
+    images
+    index
+    javascripts
+    login
+    logout
+    new
+    session
+    stylesheets
+    users
+  )
 
   #  ## Friendly Finders
   #
