07c9ad5f537b4689020f0a2a72ff4b5b213e1a1e
Andy Waite <github.aw@andywaite.com>
2014-06-22 17:31:06 +0200

    Configure database_cleaner and capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
-require 'capybara/email/rspec'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -42,16 +41,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-  end
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 
   config.include FactoryGirl::Syntax::Methods
 
