f5aba35bd205745650d11aaa9f6e84e370f3ce74
Andy Waite <github.aw@andywaite.com>
2014-06-29 21:11:09 +0200

    Starter

--- spec/lib/starter_spec.rb
+++ spec/lib/starter_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
+require 'starter'
 
 describe Starter do
-  let(:user) { create(:user) }
-
-  before do
-    Topic.create!(name: "techradar.io")
-    Starter.run(user)
-  end
-
   it "creates a sample radar" do
+    user = create(:user)
+
+    Starter.new.user_created(user)
+
     radars = user.radars
     expect(radars.count).to eq(1)
     expect(radars.first.name).to eq(described_class::DEFAULT_RADAR_NAME)
   end
 
   it "creates a sample blip" do
+    user = create(:user)
+
+    Starter.new.user_created(user)
+
     expect(user.radars.first.blips.count).to eq(1)
   end
 end
