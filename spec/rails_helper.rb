083d86befe6ba9c925397d0942b098fac840f392
Andy Waite <github.aw@andywaite.com>
2014-06-29 21:10:52 +0200

    Database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
