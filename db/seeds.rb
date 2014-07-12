382c702e077a1164a47cc3b895df2e811b4d4766
Andy Waite <github.aw@andywaite.com>
2014-07-12 12:01:46 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) can be set in the file .env file.
-puts 'DEFAULT USERS'
-raise "users exist" if User.count > 0
+raise "Users exist" if User.count > 0
 
-admin = User.create! name: ENV['ADMIN_NAME'].dup,
-  email: ENV['ADMIN_EMAIL'].dup,
-  password: ENV['ADMIN_PASSWORD'].dup,
-  password_confirmation: ENV['ADMIN_PASSWORD'].dup,
+admin = User.create!(
+  name:                  ENV['ADMIN_NAME'],
+  email:                 ENV['ADMIN_EMAIL'],
+  password:              ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD'],
   admin: true
-puts "Admin: #{admin.email}"
+)
+puts "Admin Created: #{admin.email}"
