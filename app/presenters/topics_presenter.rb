94cafc16574e7223f56c9e9e039c0d568d9d8660
Andy Waite <github.aw@andywaite.com>
2014-12-22 12:51:08 +0000

    Fix Rubocop warning

--- app/presenters/topics_presenter.rb
+++ app/presenters/topics_presenter.rb
@@ -4,10 +4,20 @@ class TopicsPresenter
   end
 
   def each_letter
-    @topics.group_by do |topic|
-      topic.name[0].upcase
-    end.each do |letter, topics|
+    grouped_topics.each do |letter, topics|
       yield letter, topics
+    end
+  end
+
+  protected
+
+  attr_reader :topics
+
+  private
+
+  def grouped_topics
+    topics.group_by do |topic|
+      topic.name[0].upcase
     end
   end
 end
