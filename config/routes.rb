d664b41fb917343c3a89d5be2e8287373634f584
Andy Waite <github.aw@andywaite.com>
2014-12-22 12:30:50 +0000

    Change home#index route to home#show
    Since this is a singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root to: "home#show"
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users, only: [:index, :show]
   resources :radars, only: [:index, :new, :create, :show, :destroy] do
