d36f7716739d3ba1d24b0d16be50dcbac920f8ac
Andy Waite <github.aw@andywaite.com>
2015-02-15 16:34:46 +0000

    Add username to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     name "Test User"
     sequence(:email) { |n| "example#{n}@example.com" }
+    sequence(:username) { |n| "User #{n}" }
     password "changeme"
     password_confirmation "changeme"
     # required if the Devise Confirmable module is used
