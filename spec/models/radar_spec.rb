d19568b2c9cdafcd140c90af633dce11da085893
Andy Waite <github.aw@andywaite.com>
2015-02-27 09:05:24 +0000

    Add missing specs

--- spec/models/radar_spec.rb
+++ spec/models/radar_spec.rb
@@ -30,11 +30,77 @@ describe Radar do
     end
   end
 
+  it "is assigns a UUID before validation" do
+    radar = build(:radar)
+    allow(SecureRandom).to receive(:uuid) { "abc123" }
+
+    radar.valid?
+
+    expect(radar.uuid).to eq("abc123")
+  end
+
+  it "doesn't change the UUID when the radar is updated" do
+    radar = create(:radar)
+    original_uuid = radar.uuid
+
+    radar.valid?
+
+    expect(radar.uuid).to eq(original_uuid)
+  end
+
+  describe "default scope" do
+    it "orders by the updated timestamp" do
+      middle = create(:radar, updated_at: 2.day.ago)
+      oldest = create(:radar, updated_at: 3.day.ago)
+      newest = create(:radar, updated_at: 1.day.ago)
+
+      result = described_class.all
+
+      expect(result).to eq([newest, middle, oldest])
+    end
+  end
+
   describe "#new_blip" do
     it "builds a new blip on the blips association" do
-      params = double
-      expect(subject.blips).to receive(:new).with(params)
+      params = double(:params)
+      allow(subject.blips).to receive(:new)
+
       subject.new_blip(params)
+
+      expect(subject.blips).to have_received(:new).with(params)
+    end
+  end
+
+  describe "#find_blip" do
+    specify do
+      radar = build(:radar)
+      topic = create(:topic, slug: "java")
+      blip = create(:blip, radar: radar, topic: topic)
+
+      result = radar.find_blip("java")
+
+      expect(result).to eq(blip)
+    end
+  end
+
+  describe "#owned_by?" do
+    specify do
+      user = build(:user)
+      radar = build(:radar, owner: user)
+
+      result = radar.owned_by?(user)
+
+      expect(result).to be(true)
+    end
+
+    specify do
+      user = build(:user)
+      another_user = build(:user)
+      radar = build(:radar, owner: user)
+
+      result = radar.owned_by?(another_user)
+
+      expect(result).to be(false)
     end
   end
 end
