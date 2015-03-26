4f7db120ce38dea696f7714d0b04026cfcfc0ccc
Andy Waite <github.aw@andywaite.com>
2015-03-26 15:56:35 +0000

    Fix Rubocop violations

--- spec/models/add_topics_spec.rb
+++ spec/models/add_topics_spec.rb
@@ -3,35 +3,35 @@ require "rails_helper"
 describe AddTopics, "#call" do
   it "adds no topics if given an empty collection" do
     creator = create(:user)
-    AddTopics.call(topics: [], creator: creator)
+    described_class.call(topics: [], creator: creator)
 
     expect(Topic.count).to eq(0)
   end
 
   it "adds a given list of topics" do
     creator = create(:user)
-    AddTopics.call(topics: ["Topic 1", "Topic 2"], creator: creator)
+    described_class.call(topics: ["Topic 1", "Topic 2"], creator: creator)
 
     expect(Topic.count).to eq(2)
   end
 
   it "skips existing topics" do
     creator = create(:user)
-    AddTopics.call(topics: ["Topic 1", "Topic 2"], creator: creator)
+    described_class.call(topics: ["Topic 1", "Topic 2"], creator: creator)
 
     expect(Topic.count).to eq(2)
   end
 
   it "strips whitespace" do
     creator = create(:user)
-    AddTopics.call(topics: ["\t Topic 1 \t "], creator: creator)
+    described_class.call(topics: ["\t Topic 1 \t "], creator: creator)
 
     expect(Topic.first.name).to eq("Topic 1")
   end
 
   it "ignores blank lines" do
     creator = create(:user)
-    AddTopics.call(topics: ["Topic 1", " "], creator: creator)
+    described_class.call(topics: ["Topic 1", " "], creator: creator)
 
     expect(Topic.count).to eq(1)
   end
