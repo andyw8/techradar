96118e930c84e78866aa5eba3cfe9b721ed1860b
Andy Waite <github.aw@andywaite.com>
2015-03-27 17:35:29 +0000

    Fix broken seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,10 @@ fail "Users exist" if User.count > 0
 admin = User.create!(
   name:                  ENV['ADMIN_NAME'],
   email:                 ENV['ADMIN_EMAIL'],
+  username:              "admin",
   password:              ENV['ADMIN_PASSWORD'],
   password_confirmation: ENV['ADMIN_PASSWORD'],
   admin: true
 )
 admin.confirm!
 puts "Admin Created: #{admin.email}"
-
-thoughtworks = User.create!(
-  name:                  "ThoughtWorks",
-  email:                 User::THOUGHTWORKS_EMAIL,
-  password:              ENV['ADMIN_PASSWORD'],
-  password_confirmation: ENV['ADMIN_PASSWORD']
-)
-thoughtworks.confirm!
-puts "ThoughtWorks Created: #{thoughtworks.email}"
