fdb7b7d37fed984d29db378e95356a87a669f523
Andy Waite <github.aw@andywaite.com>
2015-03-29 18:13:26 +0100

    Extract #already_has_topics?

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,10 @@ class User < ActiveRecord::Base
     sign_in_count == 1
   end
 
+  def already_has_topics?
+    created_topics.count > 1
+  end
+
   after_create do |user|
     publish(:user_created, user)
   end
