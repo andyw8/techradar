5f5241372416cd7f7d870f9d833e92e793a0a48b
Andy Waite <andyw8@users.noreply.github.com>
2016-03-19 11:04:23 +0000

    Improve page title for search engine indexes

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 feature "Homepage" do
   scenario do
     visit root_path
-    expect(page).to have_text("Welcome to techradar.io")
+    expect(page).to have_text("Build Your Own Technology Radar")
   end
 end
