7ac3a2b403d46457b33b2591fd88c4b8e8b7f756
Andy Waite <andyw8@users.noreply.github.com>
2017-02-20 18:22:16 -0500

    Enable bullet for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,4 +18,9 @@ Rails.application.configure do
   config.eager_load = false
   config.public_file_server.enabled = true
   config.public_file_server.headers = { "Cache-Control" => "public, max-age=3600" }
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+  end
 end
