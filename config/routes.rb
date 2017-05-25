41a16dfff12744f0fe8008a3c2a4aaf6cda91106
Andy Waite <andyw8@users.noreply.github.com>
2017-05-24 23:07:39 -0400

    Vertical whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   get "/:username", constraints: UsernameConstraint, controller: "users", action: "show"
   resources :users, only: %i[index show]
+
   resources :radars, except: :show do
     resources :blips, controller: "radars/blips", except: :index
   end
+
   get "/radars/:id(/:quadrant)", controller: "radars", action: "show", as: "radar_quadrant"
   resources :topics, only: %i[show new create]
   resources :bulk_topics, only: %i[new create]
