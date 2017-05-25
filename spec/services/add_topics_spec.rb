4b0cd87d16d1794937d8bd3bc57716b53101d859
Andy Waite <andyw8@users.noreply.github.com>
2017-05-24 23:38:06 -0400

    Use RSpec naming convention

--- spec/services/add_topics_spec.rb
+++ spec/services/add_topics_spec.rb
@@ -1,38 +1,40 @@
 require "rails_helper"
 
-describe AddTopics, "#call" do
-  before do
-    # When running the specs with `rake spec` they intermittently fail. Seems
-    # something isn't been cleared out but I can't figure out what's going on.
-    # So using this as a temporary workaround.
-    Topic.delete_all
-  end
+describe AddTopics do
+  describe "#call" do
+    before do
+      # When running the specs with `rake spec` they intermittently fail. Seems
+      # something isn't been cleared out but I can't figure out what's going on.
+      # So using this as a temporary workaround.
+      Topic.delete_all
+    end
 
-  it "adds no topics if given an empty collection" do
-    creator = create(:user)
-    described_class.call(topics: [], creator: creator)
+    it "adds no topics if given an empty collection" do
+      creator = create(:user)
+      described_class.call(topics: [], creator: creator)
 
-    expect(Topic.count).to eq(0)
-  end
+      expect(Topic.count).to eq(0)
+    end
 
-  it "adds a given list of topics" do
-    creator = create(:user)
-    described_class.call(topics: ["Topic 1", "Topic 2"], creator: creator)
+    it "adds a given list of topics" do
+      creator = create(:user)
+      described_class.call(topics: ["Topic 1", "Topic 2"], creator: creator)
 
-    expect(Topic.count).to eq(2)
-  end
+      expect(Topic.count).to eq(2)
+    end
 
-  it "strips whitespace" do
-    creator = create(:user)
-    described_class.call(topics: ["\t Topic 1 \t "], creator: creator)
+    it "strips whitespace" do
+      creator = create(:user)
+      described_class.call(topics: ["\t Topic 1 \t "], creator: creator)
 
-    expect(Topic.first.name).to eq("Topic 1")
-  end
+      expect(Topic.first.name).to eq("Topic 1")
+    end
 
-  it "ignores blank lines" do
-    creator = create(:user)
-    described_class.call(topics: ["Topic 1", " "], creator: creator)
+    it "ignores blank lines" do
+      creator = create(:user)
+      described_class.call(topics: ["Topic 1", " "], creator: creator)
 
-    expect(Topic.count).to eq(1)
+      expect(Topic.count).to eq(1)
+    end
   end
 end
