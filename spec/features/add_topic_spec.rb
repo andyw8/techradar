b1bd18b4312964291d312da1be1ac75cb08e79b6
Andy Waite <andyw8@users.noreply.github.com>
2017-06-01 19:58:00 -0400

    Restore topics controller
    Accidentally removed.

---
+++ spec/features/add_topic_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "Add Topic" do
+  let(:user) { create(:user) }
+
+  scenario do
+    radar = create(:radar, owner: user)
+    login_as(user)
+    visit radar_path(radar)
+    click_link "New Blip"
+    click_link "Add a New Topic"
+    fill_in "Name", with: "Topic 1"
+    click_button "Create Topic"
+    visit radar_path(radar)
+    click_link "New Blip"
+    expect(page).to have_css("option", text: "Topic 1")
+  end
+end
