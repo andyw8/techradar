ede8eaa62bc55bae219442535a3053fa75ef7162
Andy Waite <andyw8@users.noreply.github.com>
2017-06-18 20:35:16 -0400

    Use Chrome for tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,2 +1,21 @@
+require "selenium/webdriver"
 require "capybara/email/rspec"
+
 Capybara.asset_host = "techradar.dev"
+
+# https://robots.thoughtbot.com/headless-feature-specs-with-chrome
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu] }
+  )
+
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+                                      desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :chrome
