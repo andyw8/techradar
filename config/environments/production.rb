8f09209f33b10599ca7bbadf990fe698bf79125f
Andy Waite <andyw8@users.noreply.github.com>
2017-08-26 12:24:00 -0400

    Remove 12factor gem (not needed for Rails 5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,4 +28,14 @@ Rails.application.configure do
   }
 
   config.log_formatter = ::Logger::Formatter.new
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
